182bb2c047b293e779ef236267e4ad9ac18e5318
gpinto <giuseppe.pinto@lastminute.com>
2019-07-20 16:22:38 +0200

    initial commit

---
+++ ch01-container-initializer/src/main/java/com/giuseppe/pinto/applicationContext/TestClasspathApplicationContext.java
@@ -0,0 +1,26 @@
+package com.giuseppe.pinto.applicationContext;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestClasspathApplicationContext
+{
+/*
+  Sometimes, the application may have more than one configuration file in the classpath,
+  and, accidentally, they may write the definition for the same bean.
+  In such conditions where the application has more than one XML configuration file,
+  the later bean definition from the XML file will override the earlier bean definition.
+  It provides the advantage of writing a new bean definition to replace the previous one.
+*/
+  public static void main(String[] args)
+  {
+    ApplicationContext applicationContext =
+      new ClassPathXmlApplicationContext("beans_classpath.xml");
+    System.out.println("container created successfully from single config file");
+
+    applicationContext =
+      new ClassPathXmlApplicationContext("beans_classpath.xml", "another_beans_classpath.xml");
+    System.out.println("container created successfully from multiple config files");
+  }
+
+}
---
+++ ch01-container-initializer/src/main/java/com/giuseppe/pinto/beanFactory/TestBeanFactory.java
@@ -0,0 +1,19 @@
+package com.giuseppe.pinto.beanFactory;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class TestBeanFactory
+{
+  //BeanFactory doesn't support multiple configuration files.
+
+  public static void main(String[] args)
+  {
+    BeanFactory beanFactory =
+      new XmlBeanFactory(
+        new ClassPathResource("beans_classpath.xml"));
+    System.out.println("beanfactory created successfully");
+  }
+
+}
