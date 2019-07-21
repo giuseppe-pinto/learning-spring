009069b14f8bd662024cc17701c567d452a8d020
gpinto <giuseppe.pinto@lastminute.com>
2019-07-21 12:29:40 +0200

    repackaging and added DI modules with a first example of xml base setter injections

--- ch01-container-initializer/src/main/java/com/giuseppe/pinto/beans/applicationcontext/TestClasspathApplicationContext.java
+++ ch01-container-initializer/src/main/java/com/giuseppe/pinto/beans/applicationcontext/TestClasspathApplicationContext.java
@@ -5,15 +5,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestClasspathApplicationContext
 {
-/*
-  Sometimes, the application may have more than one configuration file in the classpath,
-  and, accidentally, they may write the definition for the same bean.
-  In such conditions where the application has more than one XML configuration file,
-  the later bean definition from the XML file will override the earlier bean definition.
-  It provides the advantage of writing a new bean definition to replace the previous one.
-*/
+  /*
+    Sometimes, the application may have more than one configuration file in the classpath,
+    and, accidentally, they may write the definition for the same bean.
+    In such conditions where the application has more than one XML configuration file,
+    the later bean definition from the XML file will override the earlier bean definition.
+    It provides the advantage of writing a new bean definition to replace the previous one.
+  */
   public static void main(String[] args)
   {
+
     ApplicationContext applicationContext =
       new ClassPathXmlApplicationContext("beans_classpath.xml");
     System.out.println("container created successfully from single config file");
